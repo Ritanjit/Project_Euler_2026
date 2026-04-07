1853d08638215f56152e5037b463ef9e6ae4569b
Ritanjit <ritanjitdas@gmail.com>
2026-04-07 12:13:13 +0530

    P6 solved!

--- P6.cpp
+++ P6.cpp
@@ -5,3 +5,32 @@ Hence, the difference between the sum of first ten natural numbers and the squar
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
 
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    long long sumSq = 0;
+    long long sqSum = 0;
+
+    // sum of squares
+    for (int i=1; i<=100; i++) {
+        sumSq += i*i;
+    }
+
+    cout << "\nThe sum of squares is = " << sumSq << "\n";
+
+    // square of sum
+    for (int i=1; i<=100; i++) {
+        sqSum += i;
+    }
+
+    sqSum = sqSum * sqSum;
+
+    cout << "The square os sum is = " << sqSum << "\n";
+
+    // difference
+    cout << "\nHence, the difference is = " << (sqSum - sumSq) << "\n";
+
+    return 0;
+}
